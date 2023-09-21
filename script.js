cb1d459e2575b245465f3c97ed27bb754fa87fcb
Alyssa Faerber <alyssa.faerber@gmail.com>
2023-09-20 21:00:43 -0600

    I think I got the colors working.

--- script.js
+++ script.js
@@ -7,30 +7,32 @@ $("#currentDay").text(dayjs().format('dddd, MMMM DD YYYY'));
 
 // Define variables
 var timeBlockEl = document.querySelector(".containter");
-var timeBlock = $(".hour");
 var parent = $(this).parents(".row");
-var currentTime = parseInt(dayjs().format("H"));
+var currentTime = dayjs().format("HH");
 
 
 // Functions
 function auditTask(){
    
-    $(".time-block").each(function (){  
-    var hourId = parseInt(parent.attr("id"));
-    if(hourId < currentTime){
-      $(this).addClass ("past");
-    }
-    else if(hourId === currentTime){
-      $(this).removeClass('past');
-      $(this).removeClass('future');
-      $(this).addClass ("present");
-    }
-    else{
-      $(this).removeClass('past');
-      $(this).removeClass('present');
-      $(this).addClass ("future");
-    }
-  })
+  $(".time-block").each(function (){  
+  var hourId = $(this).attr("id").split("-")[1];
+  
+  console.log(hourId);
+  console.log(currentTime);
+  if(hourId < currentTime){
+    $(this).addClass ("past");
+  }
+  else if(hourId === currentTime){
+    $(this).removeClass('past');
+    $(this).removeClass('future');
+    $(this).addClass ("present");
+  }
+  else{
+    $(this).removeClass('past');
+    $(this).removeClass('present');
+    $(this).addClass ("future");
+  }
+})
 }
 
 auditTask();
@@ -53,5 +55,9 @@ $("#hour-15 .description").val(localStorage.getItem("hour-15"));
 $("#hour-16 .description").val(localStorage.getItem("hour-16"));
 $("#hour-17 .description").val(localStorage.getItem("hour-17"));
 
-
+$("#clearFieldsBtn").click(function(event) {
+  event.preventDefault;
+  $("textArea").val("");
+  localStorage.clear();
+});
 
